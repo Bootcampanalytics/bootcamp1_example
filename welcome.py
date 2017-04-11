5960c6590b00337c44776f5d718218d8f15a5037
olav laudy <olavlaudy@ph.ibm.com>
2017-04-10 22:07:02 -0500

    build 1

--- welcome.py
+++ welcome.py
@@ -13,32 +13,17 @@
 # limitations under the License.
 
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def Welcome():
-    return app.send_static_file('index.html')
+def index():
+	test="hi"
+	return render_template('index.html', test=test)
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
 
-@app.route('/api/people')
-def GetPeople():
-    list = [
-        {'name': 'John', 'age': 28},
-        {'name': 'Bill', 'val': 26}
-    ]
-    return jsonify(results=list)
 
-@app.route('/api/people/<name>')
-def SayHello(name):
-    message = {
-        'message': 'Hello ' + name
-    }
-    return jsonify(results=message)
 
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
