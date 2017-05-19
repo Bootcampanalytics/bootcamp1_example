c3ed3b1f9bc325dc79cc990b9eb80ef33769e732
olav laudy <olavlaudy@ph.ibm.com>
2017-05-19 10:12:37 +0000

    new

--- run.py
+++ run.py
@@ -1,38 +1,69 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from werkzeug import secure_filename
+import json
+import pprint
+from os.path import join, dirname
 import os
+from watson_developer_cloud import DocumentConversionV1
+from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+import requests
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = '/uploads/'
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'doc', 'docx'])
+# Doc conversion
+document_conversion = DocumentConversionV1(
+  username='9b4798e3-5380-40c4-9d9f-140c34720ee5',
+  password='upIn5uG3soR7',
+  version='2015-12-15'
+)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-@app.route('/')
-def index():
-    #os.chdir("/app")
-    dir="-".join(os.listdir(os.getcwd()))
-    return render_template('index.htm',dir=dir)
+#Natural language understanding
+cred={'user':'f060dfc8-0f2d-4801-a2e1-bde638ec10b6','pass':'rvt40g4afiuF'}
+NLU_url='https://gateway.watsonplatform.net/natural-language-understanding/api/v1'
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file',filename=filename))
+@app.route('/', methods = ['GET', 'POST'])
+def serve_page():
+	if request.method == 'POST':
+		model_id = request.values.get('model_id')
+		resp=requests.delete(NLU_url+'/models/'+model_id,auth=HTTPBasicAuth(cred['user'], cred['pass']))
+		return render_template('index.htm',model_id="model deleted")
+	else:
+		resp=requests.get(NLU_url+'/models',auth=HTTPBasicAuth(cred['user'], cred['pass'])).content
+		model_id=json.loads(resp.decode('utf8'))['models'][0]['model_id']
+		return render_template('index.htm',model_id=model_id)
 
-# This route is expecting a parameter containing the name
-# of a file. Then it will locate that file on the upload
-# directory and show it on the browser, so if the user uploads
-# an image, that image is going to be show after the upload
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+@app.route('/convert', methods = ['GET', 'POST'])
+def convert_file():
+	if request.method == 'POST':
+		format = request.values.get('format')
+		config = {'conversion_target': format}
+		f = request.files['file']
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		f.save(os.path.join(basedir, f.filename))
+		with open(os.path.join(basedir, f.filename), 'rb') as document:
+			return document_conversion.convert_document(document=document,config=config).content
+		return 'there was a problem sending the file'	
+	
+
+@app.route('/analzye', methods = ['GET', 'POST'])
+def analyze_file():
+	config = {'conversion_target': 'NORMALIZED_TEXT'}
+	if request.method == 'POST':
+		f = request.files['file']
+		model_id = request.values.get('model_id')
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		f.save(os.path.join(basedir, f.filename))
+		with open(os.path.join(basedir, f.filename), 'rb') as document:
+			clear_text = document_conversion.convert_document(document=document,config=config).content
+			input_for_NLU=json.dumps({"text": clear_text,"features":{"entities": {"model": model_id}}})
+			ff=requests.post(NLU_url+'/analyze?version=2017-02-27',data=input_for_NLU,auth=HTTPBasicAuth(cred['user'], cred['pass']),headers={'content-type': 'application/json'}).content
+			return json.dumps(ff.decode('utf8'),sort_keys=True, indent=4, separators=(',', ': '))
+		return 'there was a problem sending the file'	
+	
+	
+		
 
 
 port = os.getenv('PORT', '5000')
